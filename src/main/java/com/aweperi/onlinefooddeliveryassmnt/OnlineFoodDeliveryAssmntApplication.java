31ddeb3b30f9fbb72ca2021ef023f45b2b50c5a8
Emmanuel Aweperi Adiba <eabaagah@gmail.com>
2023-06-21 01:25:05 +0000

    First commit

---
+++ src/main/java/com/aweperi/onlinefooddeliveryassmnt/OnlineFoodDeliveryAssmntApplication.java
@@ -0,0 +1,13 @@
+package com.aweperi.onlinefooddeliveryassmnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFoodDeliveryAssmntApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineFoodDeliveryAssmntApplication.class, args);
+    }
+
+}
