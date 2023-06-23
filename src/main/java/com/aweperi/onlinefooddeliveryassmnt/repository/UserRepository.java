bb2df2395392703857ab847cc06fb3f99abe8627
Emmanuel Aweperi Adiba <eabaagah@gmail.com>
2023-06-23 00:55:55 +0000

    add user repository

---
+++ src/main/java/com/aweperi/onlinefooddeliveryassmnt/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.aweperi.onlinefooddeliveryassmnt.repository;
+
+import com.aweperi.onlinefooddeliveryassmnt.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
