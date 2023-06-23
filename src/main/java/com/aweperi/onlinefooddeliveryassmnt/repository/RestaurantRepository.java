c1ef5f8d27a8a586c04dfa9eb5f428ffc4e55d66
Emmanuel Aweperi Adiba <eabaagah@gmail.com>
2023-06-23 00:51:20 +0000

    add restaurant repository

---
+++ src/main/java/com/aweperi/onlinefooddeliveryassmnt/repository/RestaurantRepository.java
@@ -0,0 +1,9 @@
+package com.aweperi.onlinefooddeliveryassmnt.repository;
+
+import com.aweperi.onlinefooddeliveryassmnt.model.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+}
