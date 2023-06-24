dcd1c02003bfa490ffe4d77d81150c54b049a88b
Emmanuel Aweperi Adiba <eabaagah@gmail.com>
2023-06-24 17:01:20 +0000

    impl generate token

--- src/main/java/com/aweperi/onlinefooddeliveryassmnt/service/JwtService.java
+++ src/main/java/com/aweperi/onlinefooddeliveryassmnt/service/JwtService.java
@@ -2,12 +2,17 @@ package com.aweperi.onlinefooddeliveryassmnt.service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -16,7 +21,7 @@ public class JwtService {
     private String SECRET_KEY;
 
     @Value("${jwt.expiration}")
-    private Long jwtExpirationDate;
+    private Long JWT_EXPIRATION;
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
@@ -25,6 +30,27 @@ public class JwtService {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails
+    ) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_EXPIRATION))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+
+
     private Claims extractAllClaims(String token) {
         return Jwts
                 .parserBuilder()
