90dd706c303e21132e61cd45790015f0bda8d518
Emmanuel Aweperi Adiba <eabaagah@gmail.com>
2023-06-24 17:29:53 +0000

    update jwt auth

--- src/main/java/com/aweperi/onlinefooddeliveryassmnt/service/UserService.java
+++ src/main/java/com/aweperi/onlinefooddeliveryassmnt/service/UserService.java
@@ -3,14 +3,16 @@ package com.aweperi.onlinefooddeliveryassmnt.service;
 import com.aweperi.onlinefooddeliveryassmnt.model.User;
 import com.aweperi.onlinefooddeliveryassmnt.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UserService implements IUserService {
+public class UserService implements IUserService, UserDetailsService {
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final JwtTokenUtil jwtTokenUtil;
+
     @Override
     public void registerUser(User user) {
 
@@ -23,6 +25,12 @@ public class UserService implements IUserService {
 
     @Override
     public User getUserByUsername(String username) {
-        return null;
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return getUserByUsername(username);
     }
 }
